d19c3a1ade9722430fb16140ec52400a380be18d
Markus <markus.j.lindgren@gmail.com>
2021-05-26 14:37:46 +0200

    more stuff

--- script.js
+++ script.js
@@ -397,21 +397,26 @@ function generateTravelEvent(){
     "You sleep under the constellation of the mage. Magic users wake up with inspiration.",
     "You sleep under the constellation of the warior. Mele players wake up with inspiration.",
     "A small creature has set up a merchant stall in the middle of nowhere. It trades treasures it as found (in nearby ruin) for food or fun trinkets.",
-    "The player taking lead makes a survival check DC " + Math.floor(Math.random() * 20) + ". On sucess find shortcut. (reroll for " + (days2travel -1) + " days)",
-    "The player taking lead makes a survival check DC " + Math.floor(Math.random() * 20) + ". On fail you get lost. (reroll for " + (days2travel + 2) + " days)",
-    "The food you ate today has gone bad. You all spend the night in agony and wake up exhausted.",
+    "The player taking lead makes a survival check DC " + (Math.floor(Math.random() * 10) + 10 ) +  ". On sucess find shortcut. (reroll for " + (days2travel -1) + " days)",
+    "The player taking lead makes a survival check DC " + (Math.floor(Math.random() * 10) + 10 ) +  ". On fail you get lost. (reroll for " + (days2travel + 2) + " days)",
+    "The food you ate today has gone bad. Con save DC" + (Math.floor(Math.random() * 10) + 10 ) +  " or spend the night in agony and wake up exhausted.",
     "A god gives a vision to one of the players when they sleep. They wake up with inspiration.",
-    "A god is testing one of the players when they sleep. Wis save DC " + Math.floor(Math.random() * 20) + "or wake up exhausted.",
+    "A god is testing one of the players when they sleep. Wis save DC " + (Math.floor(Math.random() * 10) + 10 ) +  "or wake up exhausted.",
     "The stars align and the gods smile upon you. You go to sleep and have peaceful dreams. When you wake up your exhaustion is gone.",
     "You travel trough a bountyful area. With ease you replenish your supplies with " + Math.floor(Math.random() * 6) + " days worth of rations per player.",
-    "Night watch make a " + Math.floor(Math.random() * 20) + " perception check to see animals breaking into the supplice. Fail and loose" + Math.floor(Math.random() * 6) + " days worth of rations per player.",
-    "Flies and insects keep disturbing you in your sleep. Con save " +  Math.floor(Math.random() * 20) + " or choose: only take short rest OR take long rest + a level of exhaustion.",
+    "Night watch make a " + (Math.floor(Math.random() * 10) + 10 ) +  " perception check to see animals breaking into the supplice. Fail and loose" + Math.floor(Math.random() * 6) + " days worth of rations per player.",
+    "Flies and insects keep disturbing you in your sleep. Con save " + (Math.floor(Math.random() * 10) + 10 ) + " or choose: only take short rest OR take long rest + a level of exhaustion.",
     "You come across a wounded animal in your travels, if  aided it will dissapear into the wilderness but make triumphant return to aid you in a future battle!",
     "A rare cosmic event will happen tonight. Players can choose to take one level of exhaustion to stay up late and witness it to get inspiration.",
     "The air here is foul and food tends to spoil here. Someone make a roll with 50/50 chance to spoil" + Math.floor(Math.random() * 4) + " days worth of rations per player.",
     "The party is mistaken for bandits by another adventuring party who are waiting in an ambush. They soon realize their misstake during combat and appologizes.",
     "A sign made of junk poins into the wilderness. It reads \"DIS WAE FOR DAT GOOD GOOD.\"",
-    "An animal jumps across your path. Perception check DC " + Math.floor(Math.random() * 20) + " reveals it carries a magic necklace."
+    "An animal jumps across your path. Perception check DC " + (Math.floor(Math.random() * 10) + 10 ) + " reveals it carries a magic necklace.",
+    "Your path has become ubstructed by resent bad weather. It takes 2 more days of traveling trough difficult terrain. (reroll for " + (days2travel + 1) + " days).",
+    "Your knowledge of this area appears to be outdated. The journey is easier than expected! (reroll for " + (days2travel - 2) + " days).",
+    "You find mushrooms! Nature check DC " + (Math.floor(Math.random() * 10) + 10 ) + " reveal they are poisonus. If eaten you loose 1 day of travel to aggony and stomach ache. (reroll for " + (days2travel + 1) + " days)",
+    "The full moon makes traveling in darkness easy. You make extra progress on your journey. (reroll for " + (days2travel -1) + " days) OR cover regular distance in half the time (you loose one level of exhaustion).",
+    "Freak weather conditions make travel almost impossible. Hunker down for a day (reroll for " + (days2travel -1) + " days) OR brave the condtions with a survival check DC " + (Math.floor(Math.random() * 10) + 10 ) +  "with disadvantage."
   ];
 
   return travelEventList[Math.floor(Math.random() * travelEventList.length)];
