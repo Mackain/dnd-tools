cceeb05fda0dabd4bad8d1134fe09e0b82d7ec27
Mackain <markus.j.lindgren@gmail.com>
2025-03-08 00:59:45 +0100

    ACHTUNG X-MAS!

--- quest.js
+++ quest.js
@@ -1,15 +1,16 @@
 
 const hexDiameterInMiles = 6;
-var neerDoWell = "";
-
+var questIndex = 0;
+var neerDoWell = ""
 function startQuest() {
+    // do this here to only do it once and save compute.
+    neerDoWell = getRandomNeerDoWell();
     var quest = getRandomQuestGiver() + " wants you to " + getRandomQuestTask() + ", " + getRandomQuestGoal();
-    
     // calculate dungeon location (if there is one)
     if (quest.includes("dungeon")) {
         console.log("Dungeon found!")
         quest += "</br></br><i>The dungeon is " + randomizeLocation()+"</i>";
-    } else if (neerDoWell != "") {
+    } else if (questHasNeerDoWell()) {
         console.log("No dungeon, but enemies!!")
         quest += "</br></br><i>The "+ removeFirstWord(neerDoWell) +" can most likely be found " + randomizeLocation()+"</i>";
     }
@@ -17,6 +18,15 @@ function startQuest() {
     
     printOutput(quest)
 }
+
+function questHasNeerDoWell(){
+    const specialQuests = [1, 4, 5, 8, 9, 12, 16, 17]; 
+    if (specialQuests.includes(questIndex)) {
+        return true;
+    } else {
+        return false;
+        }
+    }
 
 function printOutput(output) {
     document.getElementById("text-area").innerHTML = output;
@@ -98,28 +108,29 @@ function getQuestGiver(number) {
 }
 
 function getRandomQuestTask() {
-    return getQuestTask(Math.ceil(Math.random() * 20))
+    questIndex = Math.ceil(Math.random() * 20);
+    return getQuestTask(questIndex);
 }
 
 function getQuestTask(number) {
-    const questTaskTable = {
-        1: "slay/route out " + getRandomNeerDoWell(),
+    var questTaskTable = {
+        1: "slay/route out " + neerDoWell,
         2: "recover an artifact stolen by bandits",
         3: "escort a scholar to the inner sanctum of a dungeon where they must translate a fresco",
-        4: "defeat " +getRandomNeerDoWell()+" who moved into nearby dungeon.",
-        5: "stop " +getRandomNeerDoWell()+" from achieving their goal",
+        4: "defeat " +neerDoWell+" who moved into nearby dungeon.",
+        5: "stop " +neerDoWell+" from achieving their goal",
         6: "find their ancient family signet ring in a nearby dungeon, that will prove their nobility",
         7: "find an ancient library of lost lore in a nearby dungeon",
-        8: "find proof that " +getRandomNeerDoWell()+" is innocent and misunderstood",
-        9: "find proof that " +getRandomNeerDoWell()+" was behind a recent murder",
+        8: "find proof that " +neerDoWell+" is innocent and misunderstood",
+        9: "find proof that " +neerDoWell+" was behind a recent murder",
         10: "rescue (or at least recover the remains of) an adventurer who got lost in a dungeon",
         11: "clear out all threats in a nearby dungeon so it can be repurposed for good",
-        12: "rescue their family member from " +getRandomNeerDoWell()+"",
+        12: "rescue their family member from " +neerDoWell+"",
         13: "escort a valuable transport carrying an artifact trough dangerous lands.",
         14: "find a scroll in a nearby dungeon. But under no circumstances must you read it!",
         15: "find an artifact that was lost as it was being transported here",
-        16: "defeat " +getRandomNeerDoWell()+" and find out what they are up to",
-        17: "stop " +getRandomNeerDoWell()+" from macing a deal with a devil",
+        16: "defeat " +neerDoWell+" and find out what they are up to",
+        17: "stop " +neerDoWell+" from macing a deal with a devil",
         18: "recover an artifact lost in a dungeon",
         19: "recover a powerful grimoire, that was stolen by an evil necromancer",
         20: "slay a dragon!",
@@ -159,32 +170,31 @@ function getQuestGoal(number) {
 
 
 function getRandomNeerDoWell() {
-    neerDoWell = getNeerDoWell(Math.ceil(Math.random() * 20));
-    return neerDoWell;
+    return getNeerDoWell(Math.ceil(Math.random() * 20));
 }
 
 function getNeerDoWell(number) {
     const neerDoWellTable = {
-        1: "A group of kobolds",
-        2: "A group of goblins",
-        3: "A group of ogres",
-        4: "A group of bandits/highwaymen/pirates",
-        5: "The ghost of a evil ruler from the regions dark past",
-        6: "A vampire who was awoken from their slumber when adventurers disturbed their tomb",
-        7: "A Medusa",
-        8: "A werewolf",
-        9: "A band of orc raiders",
-        10: "A group of lizardmen who recently came to the area from elsewhere",
-        11: "A hag who has lived nearby for centuries, but only recently started to terrorize the locals.",
-        12: "A demon who has possessed a local",
-        13: "A group of deserters turned bandits, lead by their former captain",
-        14: "An ancient witch",
-        15: "A crime-lord who wants to expand their territory into the local area",
-        16: "A death cult who believes the world will end and only a powerful demon can save them",
-        17: "A mad mage, turned insane from studying forbidden knowledge",
-        18: "A necromancer with their hoard of undead",
-        19: "A secret cabal of vampires",
-        20: "A Dragon"
+        1: "a group of kobolds",
+        2: "a group of goblins",
+        3: "a group of ogres",
+        4: "a group of bandits/highwaymen/pirates",
+        5: "the ghost of a evil ruler from the regions dark past",
+        6: "a vampire who was awoken from their slumber when adventurers disturbed their tomb",
+        7: "a Medusa",
+        8: "a werewolf",
+        9: "a band of orc raiders",
+        10: "a group of lizardmen who recently came to the area from elsewhere",
+        11: "a hag who has lived nearby for centuries, but only recently started to terrorize the locals.",
+        12: "a demon who has possessed a local",
+        13: "a group of deserters turned bandits, lead by their former captain",
+        14: "an ancient witch",
+        15: "a crime-lord who wants to expand their territory into the local area",
+        16: "a death cult who believes the world will end and only a powerful demon can save them",
+        17: "a mad mage, turned insane from studying forbidden knowledge",
+        18: "a necromancer with their hoard of undead",
+        19: "a secret cabal of vampires",
+        20: "a Dragon"
     };
     return neerDoWellTable[number] || "Invalid number. Please enter a number between 1 and 20.";
 }
